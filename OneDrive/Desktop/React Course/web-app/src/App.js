513822c31dbad7a62ae214f2ccf4bcfa8d05c784
Emmanuel Baimba <emmanuelleosamuel203@gmail.com>
2023-07-16 02:46:53 +0000

    original commit

--- OneDrive/Desktop/React Course/web-app/src/App.js
+++ OneDrive/Desktop/React Course/web-app/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import NavComp from './Components/Navbar';
 import MainComp from './Components/Main';
+import FooterComp from './Components/Footer'
 const App = () => {
   return (
     <div>
       <NavComp />
       <MainComp />
+      <FooterComp />
     </div>
   )
 }
 
 export default App
+
 
---
+++ OneDrive/Desktop/React Course/web-app/src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const FooterComp = () => {
+    return(
+        <footer>
+            <small>Copyright &copy; 2023 @Emmanuel Leo Baimba| All Rights Reserved</small>
+        </footer>
+    )
+}
+export default FooterComp;
